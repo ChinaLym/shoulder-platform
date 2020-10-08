79efc76a715b1862d436fb89a880d1c6117e7ed1
15858193327 <lymdhr@qq.com>
2020-10-08 22:13:03 +0800

    fix: packageName

--- shoulder-system-center/system-start/src/main/java/cn/itlym/shoulder/platform/system/start/SystemCenter.java
+++ shoulder-system-center/system-start/src/main/java/cn/itlym/shoulder/platform/system/start/SystemCenter.java
@@ -1,4 +1,4 @@
-package cn.itlym.shoulder.platform.system.star;
+package cn.itlym.shoulder.platform.system.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
